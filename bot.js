02b5d51ab74c5a6653689ffe3b3cbaf78a3c9baa
hashtagsavege <45083111+hashtagsavege@users.noreply.github.com>
2018-11-16 18:23:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   	}
 });
 client.on('message', message => {
-    if (message.content === ';;weather') {
+    if (message.content === ';;weather1') {
     	message.reply('This feature is coming soon.');
   	}
 });
